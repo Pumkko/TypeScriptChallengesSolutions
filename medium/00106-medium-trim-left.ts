6800e0c69c670401bf5b3d8686e4bc4c00980428
Charles COQUE <Charles.Coque@cirquedusoleil.com>
2023-11-23 17:17:43 -0500

    Medium Trim Left

--- medium/00106-medium-trim-left.ts
+++ medium/00106-medium-trim-left.ts
@@ -18,7 +18,8 @@
 
 /* _____________ Your Code Here _____________ */
 
-type TrimLeft<S extends string> = any
+// Easy peasy chicken teriyaki
+type TrimLeft<S extends string> = S extends `${' ' | '\n' | '\t'}${infer Rest}` ? TrimLeft<Rest> : S
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
